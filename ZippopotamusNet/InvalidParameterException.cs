04545347b3d4a98aa3941eb9ba1583c04b14d4ae
MuffunKevin <muffunkevin@gmail.com>
2013-01-05 17:04:50 -0500

    Add exception
    Add handleling for 404 error wich mean that no result found.

---
+++ ZippopotamusNet/InvalidParameterException.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ZippopotamusNet
+{
+    /// <summary>
+    /// Define a proble with the provided search parameters.
+    /// </summary>
+    public class InvalidParameterException : Exception
+    {
+        private static string _errorMessage = "The provided parameter does not match any information";
+ 
+        public InvalidParameterException() : base(_errorMessage) { }
+
+        public InvalidParameterException(string message) : base(message) { }
+
+        public InvalidParameterException(string message, Exception e) : base(message, e) { }
+
+        public InvalidParameterException(Exception e) : base(_errorMessage, e) {}
+    }
+}
