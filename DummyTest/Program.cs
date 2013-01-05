7848db12432f863d07a765da9a937b291b26af2e
MuffunKevin <muffunkevin@gmail.com>
2013-01-04 22:50:01 -0500

    Initial
    This is the first.

---
+++ DummyTest/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using ZippopotamusNet;
+
+namespace DummyTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write(Core.ExecuteStraith("us", string.Empty, string.Empty, "90210"));
+            Console.Read();
+        }
+    }
+}
---
+++ ZippopotamusNet/Core.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net;
+using System.IO;
+
+namespace ZippopotamusNet
+{
+    //TODOS:
+    //- Make country a enum
+    //- Make overload of the ExecuteStraith function
+    //- Creaate class for each possible call of the api
+    public class Core
+    {
+        private static string _baseUrl = "http://api.hippopotam.us";
+
+        public static string ExecuteStraith(string country, string state, string city, string zipcode)
+        {
+            var result = string.Empty;
+
+            country = FormatForUrl(country);
+            state = FormatForUrl(state);
+            city = FormatForUrl(city);
+            zipcode = FormatForUrl(zipcode);
+
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(_baseUrl + country + state + city + zipcode);
+            request.Method = "GET";
+            request.ContentType = "application/json";
+
+            using (var responseStream = new StreamReader(request.GetResponse().GetResponseStream()))
+            {
+                result = responseStream.ReadToEnd();
+            }
+
+            return result;
+        }
+
+        private static string FormatForUrl(string urlPart)
+        {
+            if (!string.IsNullOrEmpty(urlPart))
+            {
+                return "/" + urlPart;
+            }
+            return urlPart;
+        }
+    }
+}
